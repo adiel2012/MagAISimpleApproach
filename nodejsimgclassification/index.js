f1071824952ba4a8cef3353a3fc5da620473aaa0
yosuna@xyncro.net <adiel2008@gmail.com>
2020-01-01 10:08:48 -0500

    Adding percentes to UI

--- nodejsimgclassification/index.js
+++ nodejsimgclassification/index.js
@@ -2,16 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-//app.use(express.staticProvider(__dirname + '/public'));
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile( path.join(__dirname,  'index.html'));
 });
-
-/*app.get('/', function(req, res) {
-    res.sendFile( path.join(__dirname, '../public',  'modelkerasimgBEST.onnx'));
-});*/
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
